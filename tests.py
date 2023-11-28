9bc71d13fbb529b473c9e2ef7fd8dd849e12d841
Bart3kD <114722169+Bart3kD@users.noreply.github.com>
2023-11-28 20:06:01 +0100

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,66 @@
+import json
+from main import app
+from unittest.mock import mock_open, patch
+from functions import load_json, get_data, find_first_free_id, insert_user, update_user, delete_user
+
+
+
+def test_load_json() -> None:
+    with patch('builtins.open', new_callable=mock_open, read_data='[{"id": 1, "name": "John", "lastname": "Doe"}]'):
+        data = load_json()
+        assert data == [{"id": 1, "name": "John", "lastname": "Doe"}]
+
+
+
+def test_get_data_all_users() -> None:
+    with patch('functions.load_json', return_value=[{"id": 1, "name": "John", "lastname": "Doe"}, {"id": 2, "name": "Jane", "lastname": "Smith"}]):
+        data = get_data()
+        assert data == [{"id": 1, "name": "John", "lastname": "Doe"}, {"id": 2, "name": "Jane", "lastname": "Smith"}]
+
+
+
+def test_get_data_single_user() -> None:
+    with patch('functions.load_json', return_value=[{"id": 1, "name": "John", "lastname": "Doe"}, {"id": 2, "name": "Jane", "lastname": "Smith"}]):
+        user_id = 1
+        data = get_data(user_id)
+        assert data == [{"id": 1, "name": "John", "lastname": "Doe"}]
+
+
+
+def test_find_first_free_id() -> None:
+    with patch('functions.load_json', return_value=[{"id": 1}, {"id": 3}, {"id": 5}]):
+        first_free_id = find_first_free_id()
+        assert first_free_id == 2
+
+
+
+def test_insert_user() -> None:
+    request_data = {'json': {'name': 'John', 'lastname': 'Doe'}}
+    
+    with app.test_request_context('/users', json=request_data['json']):
+        with patch('functions.load_json', return_value=[]):
+            with patch('functions.find_first_free_id', return_value=1):
+                response = insert_user()
+                assert response[1] == 201
+
+
+
+def test_update_user() -> None:
+    user_id = 1
+    updated_name = 'UpdatedName'
+    request_data = {'json': {'name': updated_name}}
+    
+    with app.test_request_context(f'/users/{user_id}', json=request_data['json']):
+        with patch('functions.load_json', return_value=[{"id": 1, "name": "John", "lastname": "Doe"}]):
+            response = update_user(user_id)
+            assert response[1] == 204
+
+
+
+def test_delete_user() -> None:
+    user_id = 1
+    
+    with app.test_request_context(f'/users/{user_id}'):
+        with patch('functions.load_json', return_value=[{"id": 1, "name": "John", "lastname": "Doe"}]):
+            response = delete_user(user_id)
+            assert response[1] == 204
