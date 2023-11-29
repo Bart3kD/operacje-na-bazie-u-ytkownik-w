f47a9ca22e8d70fd17b50943ba3e1e673d2ab45e
Bart3kD <114722169+Bart3kD@users.noreply.github.com>
2023-11-29 10:26:52 +0100

    added put, small changes

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from flask import Flask,request,jsonify
-from functions import get_data, insert_user, update_user, delete_user
+from functions import get_data, insert_user, update_user, delete_user, update_or_create_user
 
 app = Flask(__name__)
 
 
-@app.route("/users", methods=['GET'])
+@app.get("/users")
 def get_users():
     return jsonify(get_data())
 
-@app.route("/users/<int:user_id>", methods=['GET'])
+@app.get("/users/<int:user_id>")
 def get_user_by_id(user_id):
     return get_data(user_id)
 
-@app.route("/users",methods=['POST'])
+@app.post("/users")
 def add_users():
     insert_user(request.json)
     return "Success", 201
 
-@app.route('/users/<int:user_id>', methods=['PATCH'])
+@app.patch('/users/<int:user_id>')
 def change_user(user_id):
     return update_user(user_id)
 
-@app.route('/users/<int:user_id>', methods=['DELETE'])
+@app.put("/users/<int:user_id>")
+def edit_users(user_id):
+    return update_or_create_user(user_id)
+
+
+@app.delete('/users/<int:user_id>')
 def remove_user(user_id):
     return delete_user(user_id)
